909f5621cddf6076e087959e540bd7ce178351d1
brovatten <alexander_bridgwater@live.se>
2022-08-03 16:31:26 -0400

    preprocess to avoid name translation

--- misc/preprocess_latency.py
+++ misc/preprocess_latency.py
@@ -1,13 +1,30 @@
 import pandas as pd
 
+def name_translator(names):
+    name_translator_dic = { "US-West-N.-California" : "US-CAL-CISO",
+                            "US-East-Ohio" : "US-MIDA-PJM",
+                            "US-East-N.-Virginia" : "US-MIDW-MISO",
+                            "US-East-N.-Virginia" : "US-TEX-ERCO",
+                            "EU-Frankfurt" : "DE",
+                            "EU-Paris" : "FR",
+                            "EU-Ireland" : "IE",
+                            "EU-Milan" : "IT-NO",
+                            "EU-Stockholm" : "SE",
+                            "EU-London" : "GB" }
+    new_names = []
+    for org_name in names:
+        # Don't translate regions not used
+        if org_name not in set(name_translator_dic.keys()):
+            new_names.append(org_name)
+            continue
+        new_names.append(name_translator_dic[org_name])
+    return new_names
 
-
-df = pd.read_csv("api/cloudping/latency2.csv", index_col=None, header=None)
+df = pd.read_csv("api/cloudping/latency_unprocessed.csv", index_col=None, header=None)
 df = df.applymap(lambda x: x.replace(".00", ""))
-print(df)
 
 cols = df.iloc[:,0]
+cols = name_translator(cols)
 df = df.iloc[:, 1:]
 df.columns = cols
-
-df.to_csv("api/cloudping/latency3.csv", index=False)
+df.to_csv("api/cloudping/latency3.csv", index = False)
