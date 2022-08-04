b87035cf35eceb0cbc0d33b18151e279234e66b5
brovatten <alexander_bridgwater@live.se>
2022-08-04 15:36:37 -0400

    Requestbatch  class documentation

--- scheduler/request.py
+++ scheduler/request.py
@@ -1,5 +1,14 @@
 class RequestBatch:
+    """Imitates a batch of requests
+    """
     def __init__(self, name, load, region):
+        """
+
+        Args:
+            name: Name of request, used to debug
+            load: Load of the batch of requests
+            region: Region assigned to complete/outsource the requests
+        """
         self.name = name
         self.load = load
         self.region = region
