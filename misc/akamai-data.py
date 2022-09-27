2042757d6cc4be2f9568f945179df45423e87613
brovatten <alexander_bridgwater@live.se>
2022-09-27 19:05:57 +0200

    extracting akamai data

---
+++ misc/akamai-data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import os
+from os.path import isfile, join
+from os import listdir
+
+def main():
+    ts = 1375315200
+    ts_end = 1375401600
+    n_rows = (ts_end - ts) // 600
+    PREFIX_REQS = r"data/na/akamai_data/ECORGeoLoad/ECORGeoLoad_"
+    PREFIX_INFO = r"data/na/akamai_data/ECORInfo/"
+
+
+    load_server_info(PREFIX_INFO)
+    load_requests(PREFIX_REQS)
+
+
+def is_valid_file(file):
+    return isfile(file) and ".gz" not in file
+
+
+def load_server_info(PREFIX):
+    files = [f"{PREFIX}/{f}" for f in listdir(PREFIX) if is_valid_file(join(PREFIX, f))]
+
+    for file in files:
+        info_df = pd.read_csv(file)
+
+def load_requests(PREFIX):
+    files = [f"{PREFIX}/{f}" for f in listdir(PREFIX) if is_valid_file(join(PREFIX, f))]
+
+    for file in files:
+        info_df = pd.read_csv(file)
+
+
+
+
+main()
