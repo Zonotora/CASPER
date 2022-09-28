d47571c33aa294f3f3aecf0c7180b5e6755f3bf7
brovatten <alexander_bridgwater@live.se>
2022-09-28 16:47:10 +0200

    half-way with requests to extraction

--- misc/akamai-data.py
+++ misc/akamai-data.py
@@ -19,6 +19,8 @@ def main():
 def is_valid_file(file):
     return isfile(file) and ".gz" not in file
 
+def get_region(ecor):
+    pass
 
 def load_server_info(PREFIX):
     files = [f"{PREFIX}/{f}" for f in listdir(PREFIX) if is_valid_file(join(PREFIX, f))]
@@ -26,11 +28,21 @@ def load_server_info(PREFIX):
     for file in files:
         info_df = pd.read_csv(file)
 
-def load_requests(PREFIX):
+def load_requests(PREFIX, ecor_info):
     files = [f"{PREFIX}/{f}" for f in listdir(PREFIX) if is_valid_file(join(PREFIX, f))]
 
     for file in files:
         info_df = pd.read_csv(file)
+        for i, row in info_df.iterrows():
+
+
+def load_request_rates(PREFIX):
+    files = [f"{PREFIX}/{f}" for f in listdir(PREFIX) if is_valid_file(join(PREFIX, f))]
+
+    for file in files:
+        df = pd.read_csv(file)
+        for i, row in info_df.iterrows():
+
 
 
 
--- misc/request-rates.py
+++ misc/request-rates.py
@@ -2,37 +2,69 @@ import pandas as pd
 import numpy as np
 import os
 
+def get_ecor_region(info_df, ecor):
+    ecor_row = info_df[info_df["a.ecor"] == ecor]
+    country = str(ecor_row["b.country"])
+    if country in ["United States", "Canada"]:
+        print(str(ecor_row["b.state"]))
+        return str(ecor_row["b.state"])
+    else:
+        print(ecor_row["b.country"])
+        return str(ecor_row["b.country"])
 
 ts = 1375315200
 ts_end = 1375401600
 PREFIX = r"data/na/akamai_data/ECORGeoLoad/ECORGeoLoad_"
+INFO_PREFIX = r"data/na/akamai_data/ECORInfo/"
 
 n_rows = (ts_end - ts) // 600
 idx = pd.date_range(1375315200 * 1000 ** 3, periods=n_rows, freq="10T")
 t_stamps = idx.astype(np.int64) // 10 ** 9
 
-l = []
+l_from = []
+l_to = []
+ecor_info = {}
 for i in range(n_rows):
     try:
-        df = pd.read_csv(f"{PREFIX}{ts+600*i}_data.dat", skiprows=[1])
+        req_df = pd.read_csv(f"{PREFIX}{ts+600*i}_data.dat", skiprows=[1])
     except FileNotFoundError:
         print(f"ECORGeoLoad_{ts+600*i}_data.dat did not exist!")
-        l.append({})
+        l_from.append({})
         continue
 
-    if df.shape[0] < 3:
+    try:
+        info_df = pd.read_csv(f"{INFO_PREFIX}{ts+600*i}_data.dat.clean", skiprows=[1])
+    except FileNotFoundError:
+        print(f"ECORGeoInfo_{ts+600*i}_data.dat.clean did not exist!")
+        continue
+
+    if req_df.shape[0] < 3:
         print(f"ECORGeoLoad_{ts+600*i}_data.dat did not contain any data!")
-        l.append({})
+        l_from.append({})
         continue
 
-    df2 = df.groupby("georegion")
 
-    # Each country's summed 'sum_hits'
-    df3 = df2["sum_hits"].sum()
-    l.append(df3.to_dict())
+    #total reqs to region
+    to_df = req_df.groupby("ecor", as_index=False)["sum_hits"].sum()
+    to_df = to_df["ecor"].apply(lambda x: get_ecor_region(info_df, x))
+    print(to_df)
+    exit()
+    #total reqs from region
+    from_df = req_df.groupby("georegion")["sum_hits"].sum()
+    l_from.append(from_df.to_dict())
+    l_to.append(to_df.to_dict())
+
+    print(l_to)
+    #from is given
+    exit()
+
+    print(to_df)
+    exit()
 
 
-full_df = pd.DataFrame(l)
+
+
+full_df = pd.DataFrame(l_from)
 
 # Drop regions that dont really have any data
 full_df = full_df.drop(
